70cf53818e32ac06cad866341cc6065765c8dae9
Kisung You <kisungyou@outlook.com>
2022-01-06 22:43:47 -0500

    strategy8 complete

--- R/tphate_strategy8.R
+++ R/tphate_strategy8.R
@@ -11,38 +11,51 @@ tphate_strategy8 <- function(data, ndim=2, nbdk=5, alpha=2.0, nclust=10){
   
   # construct : affinity for data
   aff_data = aux_kernel_standard(DIST_data, round(nbdk), as.double(alpha)) 
-  
 
-  # construct : clustering-based transition
+  # construct : clustering-based transition matrix
   tmp_embed = phate_original(data, ndim=ndim, nbdk=nbdk, alpha=alpha, alg="mmds", potential="log")$embedding
-  tmp_label = T4cluster::sc05Z(tmp_embed)$cluster
-    
+  aff_lab   = T4cluster::sc05Z(tmp_embed, k=nclust, nnbd=round(nbdk))$cluster
   
-  # construct : affinity for time
-  aff_temporal = array(1,c(N,N))
+  ulabel  = unique(aff_lab)
+  nulabel = length(ulabel)
+  state_transition = array(0,c(nulabel, nulabel))
   for (i in 1:(N-1)){
-    for (j in (i+1):N){
-      aff_temporal[i,j] <- aff_temporal[j,i] <- exp((log(myc)/(N-1))*(abs(i-j)))
+    state_i = round(aff_lab[i])
+    state_j = round(aff_lab[i+1])
+    state_transition[state_i, state_j] = state_transition[state_i, state_j] + 1
+  }
+  
+  
+  
+  # construct : affinity for temporal
+  aff_temporal = array(0,c(N,N))
+  for (i in 1:N){
+    for (j in 1:N){
+      state_i = round(aff_lab[i])
+      state_j = round(aff_lab[j])
+      aff_temporal[i,j] = state_transition[state_i, state_j]
     }
   }
-  
-  # affinity aggregation
-  affinity_machine = aff_temporal*aff_data
-  
-  # construct : markov transition kernel
-  markov_hadamard = array(0,c(N,N))
+
+  # construct : two markov matrices
+  markov_data  = array(0,c(N,N))
+  markov_clust = array(0,c(N,N))
   for (i in 1:N){
-    tgt = as.vector(affinity_machine[i,])
-    markov_hadamard[i,] = tgt/base::sum(tgt)
+    tgt1 = as.vector(aff_data[i,])
+    tgt2 = as.vector(aff_temporal[i,])
+    
+    markov_data[i,] = tgt1/base::sum(tgt1)
+    markov_clust[i,]= tgt2/base::sum(tgt2)
   }
+  markov_all = markov_data%*%markov_clust
   
   # optimal transition steps
-  step_hadamard = aux_entropyrule_markov(markov_hadamard)
-  
+  markov_step = aux_entropyrule_markov(markov_all)
+
   # matrix multiplication
-  Pout = markov_hadamard
-  for (i in 1:(step_hadamard-1)){
-    Pout = markov_hadamard%*%Pout
+  Pout = markov_all
+  for (i in 1:(markov_step-1)){
+    Pout = markov_all%*%Pout
   }
   
   # STEP 4. Embedding
@@ -54,6 +67,6 @@ tphate_strategy8 <- function(data, ndim=2, nbdk=5, alpha=2.0, nclust=10){
   output = list()
   output$transition = Pout
   output$embedding  = Y
-  output$stepsize   = step_hadamard
+  output$stepsize   = markov_step
   return(output)
 }
